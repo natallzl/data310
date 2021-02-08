b66d306460b092c192eba10aeaa97a55d78f0b86
Natalie Larsen <48963641+natallzl@users.noreply.github.com>
2021-02-08 12:29:36 -0500

    Update Feb8Response.py

--- Feb8Response.py
+++ Feb8Response.py
@@ -16,6 +16,9 @@ training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
 #create model
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 model.compile(optimizer = tf.keras.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
