b7aa91c94b615badfd18db54e8614d9be96f0fac
Natalie Larsen <48963641+natallzl@users.noreply.github.com>
2021-02-10 10:13:51 -0500

    Update Feb8Response.py

--- Feb8Response.py
+++ Feb8Response.py
@@ -1,36 +1,94 @@
+#Updated code,
+  #adapted from code presented in class Feb 8
+
 import tensorflow as tf
+from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
+print(tf.__version__)
 
 #get the dataset
 mnist = tf.keras.datasets.mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-len(training_images)
 training_images.shape
 len(training_labels)
+training_labels
 test_images.shape
+len(test_labels)
 
 #normalize values
 training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
+#check out some of the training images
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i]])
+plt.show()
+
 #create model
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
                                     tf.keras.layers.Dense(128, activation=tf.nn.relu),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
 model.compile(optimizer = tf.keras.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(training_images, training_labels, epochs=5)
-model.evaluate(test_images, test_labels)
+
+model.fit(training_images, training_labels, epochs=10)
+
+#evaluate accuracy
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
 
 #create array of probabilities
-classifications = model.predict(test_images)
-print(classifications[1])
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
 
-np.argmax(classifications[1])
+predictions[1]
+np.argmax(predictions[1])
+test_labels[1]
 
-print(test_labels[1])
+#plot image and histogram
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  plt.imshow(img, cmap=plt.cm.binary)
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
 
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+i = 1
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
 plt.imshow(test_images[1])
